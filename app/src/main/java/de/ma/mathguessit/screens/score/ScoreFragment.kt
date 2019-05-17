7538132a4592732005c13fcc5ba851e3b892ae18
amatanat <ahmadova.matanat01@gmail.com>
2019-05-17 18:04:50 +0200

    Use DataBinding in all fragments.

--- app/src/main/java/de/ma/mathguessit/screens/score/ScoreFragment.kt
+++ app/src/main/java/de/ma/mathguessit/screens/score/ScoreFragment.kt
@@ -6,16 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import de.ma.mathguessit.R
+import de.ma.mathguessit.databinding.FragmentScoreBinding
 
 class ScoreFragment : Fragment() {
+
+    private lateinit var binding: FragmentScoreBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_score, container, false)
+        return binding.root
     }
 
 
--- app/src/main/java/de/ma/mathguessit/screens/title/TitleFragment.kt
+++ app/src/main/java/de/ma/mathguessit/screens/title/TitleFragment.kt
@@ -6,16 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import de.ma.mathguessit.R
+import de.ma.mathguessit.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
+
+    private lateinit var binding: FragmentTitleBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        return binding.root
     }
 
 
