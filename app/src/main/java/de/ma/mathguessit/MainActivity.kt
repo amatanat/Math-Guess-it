31096485a5e92bee3734de33f2aedd29717d246e
amatanat <ahmadova.matanat01@gmail.com>
2019-05-17 17:10:50 +0200

    Initial commit - app state.

---
+++ app/src/main/java/de/ma/mathguessit/MainActivity.kt
@@ -0,0 +1,12 @@
+package de.ma.mathguessit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
