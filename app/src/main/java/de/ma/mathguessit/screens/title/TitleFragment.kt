403505f723f7bec485015889b7ef34e5fad5851b
amatanat <ahmadova.matanat01@gmail.com>
2019-05-17 19:04:05 +0200

    Update layouts for fragments.

--- app/src/main/java/de/ma/mathguessit/screens/title/TitleFragment.kt
+++ app/src/main/java/de/ma/mathguessit/screens/title/TitleFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import de.ma.mathguessit.R
 import de.ma.mathguessit.databinding.FragmentTitleBinding
 
@@ -20,6 +21,9 @@ class TitleFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        binding.playBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_title_to_game)
+        }
         return binding.root
     }
 
